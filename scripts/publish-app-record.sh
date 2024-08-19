df67092322b2c8f7d63f157b680cedb76f6e2e60
Rysiman <97681598+rysiman@users.noreply.github.com>
2024-08-19 23:43:34 +0300

    Update publish-app-record.sh

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -9,7 +9,6 @@ CONFIG_FILE=`mktemp`
 CERC_APP_TYPE=${CERC_APP_TYPE:-"webapp"}
 CERC_REPO_REF=${CERC_REPO_REF:-${GITHUB_SHA:-`git log -1 --format="%H"`}}
 CERC_IS_LATEST_RELEASE=${CERC_IS_LATEST_RELEASE:-"true"}
-
 rcd_name=$(jq -r '.name' package.json | sed 's/null//')
 rcd_desc=$(jq -r '.description' package.json | sed 's/null//')
 rcd_repository=$(jq -r '.repository' package.json | sed 's/null//')
@@ -21,13 +20,19 @@ rcd_app_version=$(jq -r '.version' package.json | sed 's/null//')
 cat <<EOF > "$CONFIG_FILE"
 services:
   registry:
-    rpcEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-https://laconicd.laconic.com}'
-    gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-https://laconicd.laconic.com/api}'
-    chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
+    rpcEndpoint: 'https://laconicd.laconic.com'
+    gqlEndpoint: 'https://laconicd.laconic.com/api'
+    chainId: laconic_9000-1
     gas: 9550000
     fees: 15000000alnt
 EOF
 
+
+if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
+  bond_id=$(laconic -c $CONFIG_FILE registry bond create --type alnt --quantity 100000000 --user-key "${CERC_REGISTRY_USER_KEY}")
+
+  CERC_REGISTRY_BOND_ID=$(echo ${bond_id} | jq -r .bondId)
+fi
 
 next_ver=$(laconic -c $CONFIG_FILE registry record list --type ApplicationRecord --all --name "$rcd_name" 2>/dev/null | jq -r -s ".[] | sort_by(.createTime) | reverse | [ .[] | select(.bondId == \"$CERC_REGISTRY_BOND_ID\") ] | .[0].attributes.version" | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
@@ -56,6 +61,13 @@ cat $AR_RECORD_FILE
 AR_RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $AR_RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
 echo $AR_RECORD_ID
 
+if [ -z "$CERC_REGISTRY_APP_CRN" ]; then
+  authority=$(echo "$rcd_name" | cut -d'/' -f1 | sed 's/@//')
+  app=$(echo "$rcd_name" | cut -d'/' -f2-)
+  CERC_REGISTRY_APP_CRN="lrn://$authority/applications/$app"
+#  laconic -c $CONFIG_FILE registry authority reserve ${authority} --user-key "${CERC_REGISTRY_USER_KEY}"
+#  laconic -c $CONFIG_FILE registry authority bond set ${authority} ${CERC_REGISTRY_BOND_ID} --user-key "${CERC_REGISTRY_USER_KEY}"
+fi
 
 laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${rcd_app_version}" "$AR_RECORD_ID"
 laconic -c $CONFIG_FILE registry name set --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} "$CERC_REGISTRY_APP_CRN@${CERC_REPO_REF}" "$AR_RECORD_ID"
@@ -88,6 +100,10 @@ record:
 EOF
 
 cat $ADR_RECORD_FILE
+ADR_RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $ADR_RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
+echo $ADR_RECORD_ID
+
+rm -f $AR_RECORD_FILE $ADR_RECORD_FILE $CONFIG_FILE
 ADR_RECORD_ID=$(laconic -c $CONFIG_FILE registry record publish --filename $ADR_RECORD_FILE --user-key "${CERC_REGISTRY_USER_KEY}" --bond-id ${CERC_REGISTRY_BOND_ID} | jq -r '.id')
 echo $ADR_RECORD_ID
 
